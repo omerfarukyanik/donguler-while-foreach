9955a48ddca0f7574808d9c54a063bf18d16e23e
Ömer Faruk Yanık <omerfarukyanik212313@gmail.com>
2024-02-19 23:26:22 +0300

    first commit

---
+++ donguler-while-foreach/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+int sayi = int.Parse(Console.ReadLine());
+int sayac = 1;
+int toplam = 0;
+while (sayac <= sayi)
+{
+    toplam += sayac;
+    sayac++;
+}
+Console.WriteLine(toplam / sayi);
+
+char character = 'a';
+while (character <= 'z')
+{
+    Console.WriteLine(character);
+    character++;
+}
+
+Console.WriteLine("***** Foreach *****");
+string[] arabalar = { "BMV", "Ford", "Toyota", "Nissan" };
+
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
